1f34997c2740b5fd079e863e805b06cbd8c9d3a9
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-03-04 09:41:36 +0600

    optional chaining and ternary operators some problem solved

--- js/app.js
+++ js/app.js
@@ -90,36 +90,36 @@ const displayAiToolDetails = tool => {
   document.getElementById("tool-container").innerHTML = `
        <div class="card w-100 rounded-4">
            <div class="card-body bg-danger bg-opacity-10 border border-danger rounded-4">
-               <h5 class="card-title">${description}</h5>
+               <h5 class="card-title">${description ? description : "No Data Found"}</h5>
               <div class="d-flex flex-column flex-md-row justify-content-between gap-2 w-100 my-5">
                 <div class="text-center p-2 bg-white rounded-3 text-success fw-semibold">
-                <span class="">${!pricing[0]?.price || (pricing[0]?.price) === "0" || (pricing[0]?.price).includes("No cost") ? "Free of Cost/" : pricing[0]?.price}</span> <br />
-                 <span class="">${!pricing[0]?.plan || pricing[0]?.plan === "Free" ? "Basic" : pricing[0]?.plan}</span>
+                <span class="">${!pricing || (pricing[0]?.price) === "0" || (pricing[0]?.price).includes("No cost") ? "Free of Cost/" : pricing[0]?.price}</span> <br />
+                 <span class="">${!pricing || pricing[0]?.plan === "Free" ? "Basic" : pricing[0]?.plan}</span>
                  </div>
                  <div class="text-center p-2 bg-white rounded-3 fw-semibold" style="color: #F28927;">
-                 <span class="">${!pricing[1]?.price || (pricing[1]?.price) === "0" || (pricing[1]?.price).includes("No cost") ? "Free of Cost/" : pricing[1]?.price}</span> <br />
-                 <span class="">${!pricing[1]?.plan ? "Pro" : pricing[1]?.plan}</span>
+                 <span class="">${!pricing || (pricing[1]?.price) === "0" || (pricing[1]?.price).includes("No cost") ? "Free of Cost/" : pricing[1]?.price}</span> <br />
+                 <span class="">${!pricing ? "Pro" : pricing[1]?.plan}</span>
                  </div>
                  <div class="text-center p-2 bg-white rounded-3 text-danger fw-semibold">
-                 <span class="">${pricing[2]?.price ? (pricing[2]?.price).slice(0, 10) : "Free of Cost /"}</span> <br />
-                 <span class="">${pricing[2]?.plan ? pricing[2]?.plan : "Enterprise"}</span>
+                 <span class="">${pricing ? (pricing[2]?.price).slice(0, 10) : "Free of Cost /"}</span> <br />
+                 <span class="">${pricing ? pricing[2]?.plan : "Enterprise"}</span>
                  </div>
                </div>
                <div class="d-flex flex-column flex-md-row justify-content-between gap-2">
                  <div>
                    <h5>Features</h5>
                    <ul class="text-secondary">
-                    <li>${features[1]?.feature_name ? features[1]?.feature_name : "No Data Found"}</li>
-                    <li>${features[2]?.feature_name ? features[2]?.feature_name : "No Data Found"}</li>
-                    <li>${features[3]?.feature_name ? features[3]?.feature_name : "No Data Found"}</li>
+                    <li>${features ? (features[1]?.feature_name ? features[1]?.feature_name : "No Data Found") : "No Data Found"}</li>
+                    <li>${features ? (features[2]?.feature_name ? features[2]?.feature_name : "No Data Found") : "No Data Found"}</li>
+                    <li>${features ? (features[3]?.feature_name ? features[3]?.feature_name : "No Data Found") : "No Data Found"}</li>
                    </ul>
                  </div>
                  <div>
                  <h5>Integrations</h5>
                    <ul class="text-secondary">
-                    <li>${integrations[0] ? integrations[0] : "No Data Found"}</li>
-                    <li>${integrations[1] ? integrations[1] : "No Data Found"}</li>
-                    <li>${integrations[2] ? integrations[2] : "No Data Found"}</li>
+                    <li class="${!integrations ? "list-unstyled" : ""}">${integrations ? (integrations[0] ? integrations[0] : "No Data Found") : ""}</li>
+                    <li style="${!integrations ? "list-style: none; margin-left: -32px;" : ""}">${integrations ? (integrations[1] ? integrations[1] : "No Data Found") : "No Data Found"}</li>
+                    <li class="${!integrations ? "list-unstyled" : ""}">${integrations ? (integrations[2] ? integrations[2] : "No Data Found") : ""}</li>
                    </ul>
                  </div>
                </div>
@@ -129,11 +129,11 @@ const displayAiToolDetails = tool => {
         <div class="card w-100 rounded-4">
             <p id="no-accuracy" class="${accuracy?.score ? "bg-danger rounded-3 w-25 text-center p-2 fw-semibold position-absolute" : "bg-transparent"}" style="right: 25px; top: 25px;">
                 ${accuracy?.score ? (accuracy?.score).toString().substring(2) + "% accuracy" : ""}</p>
-             <img src="${image_link[0] ? image_link[0] : "No Image Found"}" class="card-img-top rounded-5 p-3" alt="...">
+             <img src="${image_link ? image_link[0] : "No Image Found"}" class="card-img-top rounded-5 p-3" alt="...">
              <div class="card-body text-center">
-                 <h5 class="card-title">${input_output_examples[0]?.input ? input_output_examples[0]?.input : "No Data Found"}</h4>
+                 <h5 class="card-title">${input_output_examples ? input_output_examples[0]?.input : "No Data Found"}</h4>
                  <p class="card-text text-secondary">
-                ${input_output_examples[0]?.output ? input_output_examples[0]?.output : "No! Not Yet! Take a break!!!"}</p>
+                ${input_output_examples ? input_output_examples[0]?.output : "No! Not Yet! Take a break!!!"}</p>
             </div>
         </div>`;
 };
